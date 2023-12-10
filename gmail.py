5c8a118d8ab52660723d39ce649a10166a9a549d
Jagruti Patil <patil.jagruti2022@gmail.com>
2023-12-11 00:19:30 +0530

    Adding few Changes

--- gmail.py
+++ gmail.py
@@ -3,31 +3,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score,classification_report
-#LoadGmailDataSet
+#Load Gmail DataSet
 data=pd.read_csv('C:\Users\DELL\Downloads\mail_data.csv')
-#splitdatasetintotrainingandtestingset
+#split data set into training and testing set
 train_data,test_data,train_labels,test_label=train_test_split(data['Message'],data['Category'],test_size=0.2,random_state=42)
-#Createcountervectorizeroftextdata
+#Create counter vectorizer of text data
 vector=CountVectorizer()
 train_vectors=vector.fit_transform(train_data)
 test_vector=vector.transform(test_data)
-print("printingtraindata")
+print("printing train data")
 print(train_vectors)
-print("printingtestdata")
+print("printing test data")
 print(test_vector)
 #TrainNAiveBayesalgorithm
 classifier=MultinomialNB()
-print("printingCalssification")
+print("printing Calssification")
 print(classifier.fit(train_vectors,train_labels))
-#Makepredictionontestdata
+#Make prediction on test data
 print(test_label)
 print(train_labels)
 predictions=classifier.predict(test_vector)
 print(predictions)
-#Evaluatethemodel
+#Evaluate the model
 accuracy=accuracy_score(test_label,predictions)
 print(accuracy)
 print('Accuracy:{:.2f}'.format(accuracy))
-
-#Displayclassificationreport
+#Display classification report
 print(classification_report(test_label,predictions))
